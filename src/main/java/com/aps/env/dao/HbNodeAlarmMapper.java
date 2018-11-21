13c0900724440e1750283bdb6a318ead11b7a213
Guanbang Liu <appleshow@hotmail.com>
2018-11-21 15:49:57 +0800

    2018-11-21

---
+++ src/main/java/com/aps/env/dao/HbNodeAlarmMapper.java
@@ -0,0 +1,30 @@
+package com.aps.env.dao;
+
+import com.aps.env.entity.HbNodeAlarm;
+import com.aps.env.entity.HbNodeAlarmExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface HbNodeAlarmMapper {
+    int countByExample(HbNodeAlarmExample example);
+
+    int deleteByExample(HbNodeAlarmExample example);
+
+    int deleteByPrimaryKey(String guid);
+
+    int insert(HbNodeAlarm record);
+
+    int insertSelective(HbNodeAlarm record);
+
+    List<HbNodeAlarm> selectByExample(HbNodeAlarmExample example);
+
+    HbNodeAlarm selectByPrimaryKey(String guid);
+
+    int updateByExampleSelective(@Param("record") HbNodeAlarm record, @Param("example") HbNodeAlarmExample example);
+
+    int updateByExample(@Param("record") HbNodeAlarm record, @Param("example") HbNodeAlarmExample example);
+
+    int updateByPrimaryKeySelective(HbNodeAlarm record);
+
+    int updateByPrimaryKey(HbNodeAlarm record);
+}
